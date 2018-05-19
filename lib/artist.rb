3b9289baa75a26a90f4757ce3c9e07c150570a6b
Kristin Ortiz <itskristin.ortiz@gmail.com>
2018-05-19 01:41:35 +0000

    automatically backed up by learn

---
+++ lib/artist.rb
@@ -0,0 +1,34 @@
+class Artist
+  attr_accessor :name
+  attr_reader :songs # the only way to add songs is by call the add_song method
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  @@all = []
+
+  def self.all
+    @@all
+  end
+
+  def self.find_or_create_by_name(name)
+    self.find_by_name(name) || self.create_by_name(name)
+    artist.name == name
+  end
+
+  def self.find_by_name(name)
+  end
+
+  def self.create_by_name(name)
+  end
+
+  def add_song(song)
+    songs.push(song)
+  end
+
+  def save
+    @@all << self
+  end
+end
